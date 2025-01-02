87718982b854267e0f914d24973b219d00540ae8
ferrycorp <152413918+ferrycorp@users.noreply.github.com>
2025-01-02 11:53:28 +0100

    correction model.sql

--- sql/model.sql
+++ sql/model.sql
@@ -10,6 +10,18 @@ CREATE TABLE specialite (
     nom_specialite VARCHAR(50) NOT NULL
 );
 
+-- Création de la table lieu
+CREATE TABLE lieu (
+    code_postal VARCHAR(5) PRIMARY KEY,
+    ville VARCHAR(50) NOT NULL
+);
+
+-- Création de la table horraire
+CREATE TABLE horraire (
+    id_horraire SERIAL PRIMARY KEY,
+    heure_debut VARCHAR(10) NOT NULL
+);
+
 -- Création de la table medecins
 CREATE TABLE medecins (
     id_medecins SERIAL PRIMARY KEY,
@@ -19,8 +31,8 @@ CREATE TABLE medecins (
     email_medecins VARCHAR(128) NOT NULL,
     mot_de_passe VARCHAR(255) NOT NULL,
     lieu VARCHAR(5) REFERENCES lieu(code_postal) ON DELETE CASCADE,
-    specialite_id VARCHAR(10) REFERENCES specialite(id_specialite) ON DELETE CASCADE,
-    horraire VARCHAR(10) REFERENCES horraire(id_horraire) ON DELETE CASCADE
+    specialite_id INT REFERENCES specialite(id_specialite) ON DELETE CASCADE,
+    horraire INT REFERENCES horraire(id_horraire) ON DELETE CASCADE
 );
 
 -- Création de la table patients
@@ -30,27 +42,15 @@ CREATE TABLE patients (
     prenom_patients VARCHAR(50) NOT NULL,
     telephone_patiens VARCHAR(15) NOT NULL,
     email_patiens VARCHAR(128) NOT NULL,
-    mot_de_passe VARCHAR(255) NOT NULL,
-);
-
--- Création de la table lieu
-CREATE TABLE lieu (
-    code_postal VARCHAR(5) PRIMARY KEY,
-    ville VARCHAR(50) NOT NULL
+    mot_de_passe VARCHAR(255) NOT NULL
 );
 
 -- Création de la table rendezvous
 CREATE TABLE rendezvous (
     id_rendezvous SERIAL PRIMARY KEY,
     commentaire VARCHAR(255),
-    patients_id VARCHAR(10) REFERENCES patients(id_patients) ON DELETE CASCADE,
-    medecin_id VARCHAR(10) REFERENCES medecins(id_medecins) ON DELETE CASCADE,
+    patients_id INT REFERENCES patients(id_patients) ON DELETE CASCADE,
+    medecin_id INT REFERENCES medecins(id_medecins) ON DELETE CASCADE,
     code_postal VARCHAR(5) REFERENCES lieu(code_postal) ON DELETE CASCADE,
-    horraire VARCHAR(10) REFERENCES horraire(id_horraire) ON DELETE CASCADE
-);
-
--- Création de la table horraire
-CREATE TABLE horraire (
-    id_horraire SERIAL PRIMARY KEY,
-    heure_debut VARCHAR(10) NOT NULL,
+    horraire INT REFERENCES horraire(id_horraire) ON DELETE CASCADE
 );
